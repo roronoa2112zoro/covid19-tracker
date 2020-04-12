770d2b72cdd8d89c2a01c4546b3a7260ec49720d
roronoa2112zoro <sahilmunshi98@gmail.com>
2020-04-12 23:19:03 +0530

    Updates

---
+++ precache-manifest.5d04fd003c1f07188f8f7c1ff0459d47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bae8e311878e2a19b8e95cbbc5e7a34",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "07089c5c31d493a3562b",
+    "url": "/covid19-tracker/static/css/main.215c6e8b.chunk.css"
+  },
+  {
+    "revision": "3e7b37fae6b1bf324469",
+    "url": "/covid19-tracker/static/js/2.035ea6cf.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.035ea6cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07089c5c31d493a3562b",
+    "url": "/covid19-tracker/static/js/main.12be7458.chunk.js"
+  },
+  {
+    "revision": "3472a32df87a999f1966",
+    "url": "/covid19-tracker/static/js/runtime-main.763ff21b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
